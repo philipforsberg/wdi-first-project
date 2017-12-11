ca6383a29869fca2b516ba208a7355822393fed5
Philip <philippernf@gmail.com>
2017-12-11 11:55:55 +0000

    added soundeffects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 // alert('Loaded JS');
+
+
+$(() => {
+
+
+  const boxChosen = [];
+
+  const $audio = $('#audio').get(0);
+  const $buttons = $('.soundbox');
+
+
+  function checkForMatch() {
+    if (boxChosen[0] === boxChosen[1]){
+      alert('Match!');
+    }
+  }
+
+
+  function playSound(e) {
+    const filename = $(e.target).attr('data-id');
+    $audio.src = `sounds/${filename}.wav`;
+    $audio.play();
+    console.log(filename);
+    $(e.target).attr('src', 'images/music.png');
+    boxChosen.push(filename);
+    if (boxChosen.length === 2) {
+      checkForMatch();
+    }
+  }
+
+
+
+  $buttons.on('click', playSound);
+
+
+
+
+
+
+  // var flipCard = function(){
+  //   var cardId = this.getAttribute('data-id');
+  //   this.setAttribute('src', (cards[cardId].cardImage));
+  //   if (cardsInPlay.length === 2) {
+  //     checkForMatch();
+  //   }
+  //   cardsInPlay.push(cards[cardId].rank);
+  // };
+
+
+
+
+
+});
