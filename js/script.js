68205972e252d531c5bd08aa17a9d62100a2c534
Philip <philippernf@gmail.com>
2017-12-11 18:36:02 +0000

    added function to randomize li's

--- js/script.js
+++ js/script.js
@@ -10,11 +10,46 @@ $(() => {
   let boxChosen = [];
 
 
-  for(let i = 0; i < $boxes.length; i++){
-    const target = Math.floor(Math.random() * $boxes.length -1) + 1;
-    const target2 = Math.floor(Math.random() * $boxes.length -1) +1;
-    $boxes.eq(target).before($boxes.eq(target2));
-  }
+  // for(let i = 0; i < $boxes.length; i++){
+  //   const target = Math.floor(Math.random() * $boxes.length -1) + 1;
+  //   const target2 = Math.floor(Math.random() * $boxes.length -1) +1;
+  //   $boxes.eq(target).before($boxes.eq(target2));
+  // }
+  //
+  // const dataIdArray = ['brickbreak', 'brickbreak', 'pause', 'pause', 'mariodies', 'mariodies', 'fireball', 'fireball', 'jump', 'jump','oneup', 'oneup', 'pipe', 'pipe', 'coin', 'coin', 'yoshi', 'yoshi', 'mario', 'mario'];
+  //
+  // function randomId() {
+  //   for (let i=0; i < $boxes.length; i++) {
+  //     const randomNumber = Math.floor(Math.random()) * $boxes.length;
+  //
+  //   }
+  // }
+
+
+  // randomId();
+
+  $('ul').each(function(){
+    // get current ul
+    var $ul = $(this);
+    // get array of list items in current ul
+    var $liArr = $ul.children('li');
+    // sort array of list items in current ul randomly
+    $liArr.sort(function(a,b){
+      // Get a random number between 0 and 10
+      var temp = parseInt( Math.random()*10 );
+      // Get 1 or 0, whether temp is odd or even
+      var isOddOrEven = temp%2;
+      // Get +1 or -1, whether temp greater or smaller than 5
+      var isPosOrNeg = temp>5 ? 1 : -1;
+      // Return -1, 0, or +1
+      return( isOddOrEven*isPosOrNeg );
+    })
+    // append list items to ul
+      .appendTo($ul);
+  });
+
+
+
 
 
 
@@ -22,7 +57,7 @@ $(() => {
     const numItems = $('.correct').length;
     console.log(numItems);
     if (numItems === 20) {
-
+      alert('Winner!');
     }
   }
 
